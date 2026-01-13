676e1653c65168e28b8ecbbec31d6076c9de2a00
YunheeAhn <ayh95944674@gmail.com>
2026-01-13 21:04:24 +0900

    feat : 기록, 마이페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React, { Suspense, useState } from 'react'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import React, { Suspense } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-const AppLayout = React.lazy(() => import('./laylouts/AppLayout'));
-const Main = React.lazy(() => import('./pages/main/Main'));
+const AppLayout = React.lazy(() => import("./laylouts/AppLayout"));
+const Main = React.lazy(() => import("./pages/main/Main"));
+const Record = React.lazy(() => import("./pages/record/Record"));
+const Mypage = React.lazy(() => import("./pages/mypage/Mypage"));
+
+// 메인 '/'
+// 기록 '/record'
+// 마이페이지 '/maypage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path='/' element={<AppLayout></AppLayout>}>
+        <Route path="/" element={<AppLayout></AppLayout>}>
           <Route index element={<Main></Main>}></Route>
+          <Route path="/record" element={<Record></Record>}></Route>
+          <Route path="/mypage" element={<Mypage></Mypage>}></Route>
         </Route>
       </Routes>
     </Suspense>
-  )
+  );
 }
 
-export default App
+export default App;
